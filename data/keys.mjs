d629a4fd2ba84789184b37dbee4a681a42e89356
Andressa <130194228+andressaxxx@users.noreply.github.com>
2023-05-30 22:49:03 -0300

    Add files via upload

---
+++ data/keys.mjs
@@ -0,0 +1,20 @@
+let keys = [ 
+    [ "decryption", {
+        "a": /ai/g,
+        "e": /enter/g,
+        "i": /imes/g,
+        "o": /ober/g,
+        "u": /ufat/g
+        }   
+    ], 
+    [ "cryptography", [
+        ["a", "ai"],
+        ["e", "enter"],
+        ["i", "imes"],
+        ["o", "ober"],
+        ["u", "ufat"],
+        ]
+    ]
+];
+
+export default keys;
